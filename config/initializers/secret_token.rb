93259dd7e2e6dd6dabc822d08afc97b19adf8e93
RustyNail <takonoyawarakaage@yahoo.co.jp>
2016-12-01 10:49:14 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyXL::Application.config.secret_key_base = '293335d66eaf0261b94a19169a5f5e5a98d54242bd3077f3e420a2053dada282f0347d47806ed5bf8daec1a1fd2b74cdafbb0351d9dd816d481f12a591d0dff1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyXL::Application.config.session_store :cookie_store, key: '_rubyXL_session'
